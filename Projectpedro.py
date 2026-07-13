b28b8075c0af31919b60429f455442a9fb909a2b
Pedroph185 <wirelesspedro@icloud.com>
2026-07-13 01:48:27 -0300

    Corrige sintaxe do Python

--- Projectpedro.py
+++ Projectpedro.py
@@ -7,23 +7,16 @@ pd.set_option("display.max_columns", None)
 
 # Faturamento por loja
 faturamento = tabela_vendas.groupby("ID Loja")["Valor Final"].sum()
-print("\n=== FATURAMENTO POR LOJA ===")
-print(faturamento.to_string())
+print(faturamento)
 
-print("\n" + "="*50 + "\n")
+print(tabela_vendas)
 
 # Quantidade de produtos vendidos
 quantidade = tabela_vendas.groupby("ID Loja")["Quantidade"].sum()
-print("\n=== QUANTIDADE POR LOJA ===")
-print(quantidade.to_string())
+print(quantidade)
 
-print("\n" + "="*50 + "\n")
+print("-"*50)
 
 # Ticket medio de produtos por loja
 ticket_medio = faturamento / quantidade
-print("\n=== TICKET MÉDIO POR LOJA ===")
-print(ticket_medio.to_string())
-
-print("\n" + "="*50 + "\n")
-
-# Enviar um e-mail como relatorio
+print(ticket_medio)
